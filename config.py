e226ebaa24921c2287d96926c97cf3c36ea0a7a5
princepia161 <42060271+princepia161@users.noreply.github.com>
2026-01-17 15:28:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,26 +2,26 @@ import os
 
 class Config(object):
     # Telegram Bot ka token
-    BOT_TOKEN = "8338892359:AAFQ9LctB2s_Efp3ULPLdW9RvOVFCHYWIHg8338892359:AAFQ9LctB2s_Efp3ULPLdW9RvOVFCHYWIHg"
+    BOT_TOKEN = "8564398983:AAHuuntq53gVgwIxQash63fatvHq-27BJTc"
     # Telegram API ki ID
-    API_ID = 35822069
+    API_ID = 20807000
     # Telegram API k
-    API_HASH = "35cea1c5e6384f57e914ac982ff5ffd1"
-    ADMIN_ID = 5938871512
+    API_HASH = "cde2366a7c61e23f4cb44618cbe6cf70"
+    ADMIN_ID = 890749443
     # MongoDB database ka URL
     DB_URL = "mongodb+srv://princepia161_db_user:KZWhQUyEe59dF1jh@cluster0.uhoxe0h.mongodb.net/?appName=Cluster0"
     # Database ka naam
     DB_NAME = "princepiaxx_bot"
     # Text log channel ki ID
-    TXT_LOG = -1002758652479
+    TXT_LOG = -1003646612944
     # Authentication log channel ki ID
-    AUTH_LOG = -1003529814222
+    AUTH_LOG = -1003646612944
     # Hit log channel ki ID
-    HIT_LOG = -1003529814222
+    HIT_LOG = -1003646612944
     # DRM dump channel ki ID
-    DRM_DUMP = -1003529814222
+    DRM_DUMP = -1003646612944
     # Main channel ki ID
-    CHANNEL = -1003529814222
+    CHANNEL = -1003646612944
     # Channel ka link
     CH_URL = "https://t.me/princepia"
     # Bot ke owner ka Telegram link
